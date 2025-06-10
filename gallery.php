ff104e968f878eb4fdd16bda7c443ffca3b454bf
diaby_i <d.ibrahim2016@gmail.com>
2025-06-11 00:15:46 +0200

    [MODIFY]: Need to add some section into index.php. And start to handle backend

--- gallery.php
+++ gallery.php
@@ -63,26 +63,58 @@
 			<!-- blog sec of the page -->
 			<!-- testimonail sec of the page -->
 			<!-- client sec of the page -->
-			<div class="container">
-				<div class="gallery col-12">
-						<img src="https://placehold.co/250x250" alt="Picture" class="img-thumbnail">
-						<img src="https://placehold.co/250x250" alt="Picture" class="img-thumbnail">
-						<img src="https://placehold.co/250x250" alt="Picture" class="img-thumbnail">
-						<img src="https://placehold.co/250x250" alt="Picture" class="img-thumbnail">
-						<img src="https://placehold.co/250x250" alt="Picture" class="img-thumbnail">
+			<!-- Shop list of the page -->
+			<div class="shop-list container">
+				<div class="row holder">
+					<div class="col-xs-12">
+						<ul class="list-unstyled drop-down">
+							<li>
+								<a href="#">Toutes les catégories <i class="fa fa-angle-down"></i></a>
+								<ul class="list-unstyled drop-down2">
+									<li><a href="#">Atelier CV</a></li>
+									<li><a href="#">Football</a></li>
+									<li><a href="#">Barbecue</a></li>
+								</ul>
+							</li>
+						</ul>
+						<span class="txt2 text-uppercase">1–12 sur 36 résultats</span>
+					</div>
 				</div>
-				<div class="row pagination-content">
-						<article id="content" class="content">
-							<!-- nav pagination of the page -->
-							<ul class="py-4 nav-pagination list-unstyled text-center">
-								<li class="prev"><a href="#">Précédent</a></li>
-								<li><a href="#">1</a></li>
-								<li class="active"><a href="#">2</a></li>
-								<li><a href="#">3</a></li>
-								<li><a href="#">4</a></li>
-								<li class="next"><a href="#">Suivant</a></li>
-							</ul>
-						</article>
+				<div class="row">
+					<?php
+					for ($i = 0; $i < 3; $i++) {
+					for ($j = 0; $j < 4; $j++) {
+
+						?>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<!-- Shop of the page -->
+							<div class="shop text-center">
+								<div class="img-holder">
+									<img src="https://placehold.co/270x270" alt="image description" class="img-responsive">
+									<span class="offer text-uppercase text-center">Tag</span>
+									<a href="#" class="cart text-center text-uppercase"> Afficher</a>
+								</div>
+								<strong class="price">Titre</strong>
+								<span class="txt"><a href="shop-detail.html">Description</a></span>
+							</div>
+						</div>
+						<?php
+					}
+				}
+					?>
+					<div class="row">
+					<div class="col-xs-12">
+						<!-- Pagination of the page -->
+						<ul class="nav-pagination list-unstyled text-center">
+							<li class="prev"><a href="#">Précédent</a></li>
+							<li><a href="#">1</a></li>
+							<li class="active"><a href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li><a href="#">4</a></li>
+							<li class="next"><a href="#">Suivant</a></li>
+						</ul>
+					</div>
+				</div>
 				</div>
 			</div>
 		</main>
